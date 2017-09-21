25875aff875f7796bbf829e1ec90cc28a017eaa3
Vojtech Juranek <vjuranek@redhat.com>
2017-09-21 22:21:00 +0200

    Add view diff into membership listener

--- jgroups-simple/src/main/java/com/github/vjuranek/jgroups/EchoReciever.java
+++ jgroups-simple/src/main/java/com/github/vjuranek/jgroups/EchoReciever.java
@@ -2,23 +2,45 @@ package com.github.vjuranek.jgroups;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jgroups.Address;
 import org.jgroups.Message;
 import org.jgroups.Receiver;
 import org.jgroups.View;
 
 public class EchoReciever implements Receiver {
-    
-    private static final Logger LOG = LogManager.getLogger(JGroupsSimple.class);
-    
-    public void viewAccepted(View newView) {
-        LOG.info("view: " + newView);
-        System.out.println("view: " + newView);
-    }
+
+    private static final Logger LOG = LogManager.getLogger(EchoReciever.class);
+    private View currentView = null;
 
     public void receive(Message msg) {
         LOG.info("<< " + msg.getObject() + " [" + msg.getSrc() + "]");
-        System.out.println("<< " + msg.getObject() + " [" + msg.getSrc() + "]");
     }
 
+    @Override
+    public void viewAccepted(View newView) {
+        LOG.info(String.format("Got view with members %s", nodesToString(newView.getMembersRaw())));
+        Address[][] viewDiff = View.diff(currentView, newView);
+        LOG.info(String.format("Joined nodes:  %s", nodesToString(viewDiff[0])));
+        LOG.info(String.format("Nodes left:  %s", nodesToString(viewDiff[1])));
+        currentView = newView;
+    }
 
+    @Override
+    public void suspect(Address suspected) {
+        LOG.info("Suspected %s", suspected.toString());
+    }
+
+    private String nodesToString(Address[] nodes) {
+        if (nodes == null || nodes.length == 0) {
+            return "(no nodes)";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (Address adr : nodes) {
+            sb.append(adr.toString()).append(',');
+        }
+        sb.replace(sb.length() - 1, sb.length(), "]");
+        return sb.toString();
+    }
 }
